fb584c5088f233364bed3ff0f62ff3081765c546
tommy santos <tommy1santos@yahoo.com>
2023-09-28 13:08:01 -0400

    Finished variables exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            string dogName = "Bobby";
-        }
-    }
-}
+﻿// Declaring and initialize variables
+string name = "Tommy";
+int age = 31;
+char middleInitial = 'D';
+bool isOver18 = true;
+double currentTemp = 91.3;
+decimal currentPrice = 29.95m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial }");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
+             
+             
+
+                  
+
